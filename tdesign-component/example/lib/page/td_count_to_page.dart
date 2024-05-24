4ce509dcaef684b3445c99699fc9d7e093a2eeb1
ccXxx1aoBai <1426169428@qq.com>
2024-05-24 18:07:44 +0800

    CountTo组件属性修改

--- tdesign-component/example/lib/page/td_count_to_page.dart
+++ tdesign-component/example/lib/page/td_count_to_page.dart
@@ -1,4 +1,10 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
+
+import 'package:flutter/animation.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/scheduler/ticker.dart';
+import 'package:flutter/widgets.dart';
 import 'package:tdesign_flutter/tdesign_flutter.dart';
 
 import '../annotation/demo.dart';
@@ -16,16 +22,43 @@ class TDCountToPage extends StatelessWidget {
           desc: "用于展示数字动画",
           exampleCodeGroup: "countTo",
           children: [
-            ExampleModule(title: "基础功能", children: [
+            ExampleModule(title: "组件类型", children: [
               ExampleItem(
-                ignoreCode: true,
-                desc: '时分秒',
+                desc: '正计数',
                 center: false,
                 padding: const EdgeInsets.only(left: 16),
                 builder: (BuildContext context) {
                   return const CodeWrapper(builder: _buildSimple);
                 },
+              ),
+
+              ExampleItem(
+                desc: '倒计数',
+                center: false,
+                padding: const EdgeInsets.only(left: 16),
+                builder: (BuildContext context) {
+                  return const CodeWrapper(builder: _buildReverse);
+                },
+              ),
+
+              ExampleItem(
+                desc: '带小数',
+                center: false,
+                padding: const EdgeInsets.only(left: 16),
+                builder: (BuildContext context) {
+                  return const CodeWrapper(builder: _buildDecimals);
+                },
               )
+            ]),
+
+            ExampleModule(title: "自定义组件", children: [
+              ExampleItem(
+                desc: '动画类型',
+                center: false,
+                padding: const EdgeInsets.only(left: 16),
+                builder: (BuildContext context) {
+                  return const CodeWrapper(builder: _buildAnimation);
+                },)
             ])
           ],
         )
@@ -35,5 +68,29 @@ class TDCountToPage extends StatelessWidget {
 
 @Demo(group: 'countTo')
 Widget _buildSimple(BuildContext context) {
-  return const TDCountTo(start: 0, end: 3000, duration: Duration(seconds: 3));
+  return const TDCountTo(start: 0, end: 3000, duration: Duration(seconds: 1));
+}
+
+@Demo(group: 'countTo')
+Widget _buildReverse(BuildContext context) {
+  return const TDCountTo(start: 0, end: 2000, duration: Duration(seconds: 1), style: TDCountToStyle.reverse);
+}
+
+@Demo(group: 'countTo')
+Widget _buildDecimals(BuildContext context) {
+  return const TDCountTo(start: 0, end: 2568.58, duration: Duration(seconds: 10), decimals: 2);
+}
+
+@Demo(group: 'countTo')
+Widget _buildAnimation(BuildContext context) {
+  return const TDCountTo(start: 0, end: 2568.58, decimals: 2, duration: Duration(seconds: 10), curve: Curves.easeInOut);
+}
+
+@Demo(group: 'countTo')
+Widget _buildController(BuildContext context) {
+  var controller = AnimationController(
+      vsync: vsync,
+      duration: const Duration(seconds: 1)
+  );
+  return const TDCountTo(start: 0, end: 1500, decimals: 2, duration: Duration(seconds: 10), curve: Curves.easeInOut, controller: controller);
 }
--- tdesign-component/lib/src/components/count_to/td_count_to.dart
+++ tdesign-component/lib/src/components/count_to/td_count_to.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
+
+enum TDCountToStyle {
+  normal, // 正计数
+  reverse, // 倒计数
+}
 
 class TDCountTo extends StatefulWidget {
   const TDCountTo({
@@ -6,16 +12,40 @@ class TDCountTo extends StatefulWidget {
     this.start = 0,
     required this.end,
     required this.duration,
+    this.controller,
+    this.decimals = 0,
+    this.textStyle,
+    this.auto = true,
+    this.style = TDCountToStyle.normal,
+    this.curve = Curves.linear,
   }) : super(key: key);
 
   /// 起始值
-  final int start;
+  final double start;
 
   /// 结束值
-  final int end;
+  final double end;
 
   /// 动画时长
   final Duration duration;
+
+  /// controller 用于用户控制动画
+  final AnimationController? controller;
+
+  /// 小数位
+  final int decimals;
+
+  /// 文字样式
+  final TextStyle? textStyle;
+
+  /// 自动播放动画
+  final bool? auto;
+
+  /// 计数类型
+  final TDCountToStyle? style;
+
+  /// 动画效果
+  final Curve? curve;
 
   @override
   _TDCountToState createState() => _TDCountToState();
@@ -25,17 +55,32 @@ class TDCountTo extends StatefulWidget {
 class _TDCountToState extends State<TDCountTo> with SingleTickerProviderStateMixin {
 
   late AnimationController _controller;
-  late Animation<int> _animation;
+  late Animation<double> _curvedAnimation;
+  late Tween<double> _tween;
+  double value = 0;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
+    _controller = widget.controller ?? AnimationController(
       vsync: this,
-      duration: widget.duration,
-    );
-    _animation = IntTween(begin: widget.start, end: widget.end).animate(_controller);
-    _controller.forward();
+      duration: widget.duration
+    )..addListener(() {
+      setState(() {
+        value = _curvedAnimation.value;
+      });
+    });
+    var start = widget.start;
+    var end = widget.end;
+    if(widget.style == TDCountToStyle.reverse) {
+      start = widget.end;
+      end = widget.start;
+    }
+    _tween = Tween(begin: start, end: end);
+    _curvedAnimation = CurvedAnimation(parent: _controller, curve: widget.curve!).drive(_tween);
+    if(widget.auto! && widget.controller == null) {
+      _controller.forward();
+    }
   }
 
   @override
@@ -47,9 +92,9 @@ class _TDCountToState extends State<TDCountTo> with SingleTickerProviderStateMix
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: _animation,
+      animation: _curvedAnimation,
       builder: (BuildContext context, Widget? child) {
-        return Text('${_animation.value}', style: const TextStyle(fontSize: 18),);
+        return Text('${value.toStringAsFixed(widget.decimals)}', style: widget.textStyle);
       },
     );
   }
